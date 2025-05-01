5f5e34ddc1927df7405e2aa33f7d71618c5376ef
Maksym Levchenko <126081409+FromMeHR@users.noreply.github.com>
2025-05-01 12:40:05 +0300

    Revert "Create "Movie" page #3"

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "../components/layouts/MainLayout";
 import { HomePage } from "../pages/HomePage/HomePage";
-import MoviePage from "../pages/MoviePage/MoviePage";
 
 export const router = createBrowserRouter([
   {
@@ -14,10 +13,6 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
-      },
-      {
-        path: "/movie/:id",
-        element: <MoviePage />,
       },
     ],
   },
